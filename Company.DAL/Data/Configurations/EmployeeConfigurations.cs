1303939d45638134a648fd4e587ff5c7f306b601
alamyrayman@outlook.sa <alamyrayman@outlook.sa>
2025-03-11 12:20:15 +0200

    IMplementation  Of DAL  For Emp Service

---
+++ Company.DAL/Data/Configurations/EmployeeConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data.Configurations
+{
+    //public class EmployeeConfigurations : IEntityTypeConfiguration<Employee>
+    //{
+    //    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Employee> builder)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+    public class EmployeeConfigurations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(e => e.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
