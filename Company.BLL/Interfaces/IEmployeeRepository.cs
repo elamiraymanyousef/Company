64a0ad63a9d8631139cd251bc78431ee0ab3ced5
hp <hp@DESKTOP-6E2N1Q9>
2025-03-11 05:42:55 -0700

    Emplemention of BLL For Employee Service Befor Genaric

---
+++ Company.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.DAL.Models;
+
+namespace Company.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? Get(int id);
+        int Add(Employee department);
+        int Update(Employee department);
+        int Delete(Employee department);
+    }
+}
---
+++ Company.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.BLL.Interfaces;
+using Company.DAL.Data.Contexts;
+using Company.DAL.Models;
+
+namespace Company.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _companyDb;
+
+        public EmployeeRepository(CompanyDbContext companyDb)
+        {
+            _companyDb = companyDb;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _companyDb.Employees.ToList();
+        }
+
+        public Employee? Get(int id)
+        {
+            return _companyDb.Employees.Find(id);
+        }
+
+        public int Update(Employee department)
+        {
+            _companyDb.Employees.Update(department);
+            return _companyDb.SaveChanges();
+        }
+
+        public int Add(Employee department)
+        {
+            _companyDb.Employees.Add(department);
+            return _companyDb.SaveChanges();    
+        }
+
+        public int Delete(Employee department)
+        {
+            _companyDb.Employees.Remove(department);
+            return _companyDb.SaveChanges();
+        }
+
+       
+    }
+}
